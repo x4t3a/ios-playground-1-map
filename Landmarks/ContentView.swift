0cf69ce14a55b082d7a125d6a0c49ef9a4d61abc
Andrey Dunaev <0h4t3a@gmail.com>
2020-02-25 20:25:21 +0300

    VStack, HStack

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,9 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock!")
-            .font(.title)
-            
+        VStack(alignment: .leading) {
+            Text("Turtle Rock!")
+                .font(.title)
+            HStack {
+                Text("Швейцария")
+                    .font(.subheadline)
+                Spacer()
+                Text("Нежный Новгород")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 
